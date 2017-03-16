7de4f21fb1a91271fe16370eb8b57c6b6f23fb5e
BalaWatermelon <jackson1994y+git@gmail.com>
2017-03-17 01:12:30 +0800

    Fixed utf8 problem

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+# coding=utf-8
 from bs4 import BeautifulSoup
 import requests
 
 
 class Calculate():
     web = requests.get('http://bd.youxidudu.com/npc/20170102/4266.html')
-    soup = BeautifulSoup(web.text.encode('utf8'), 'html.parser')
-    print(soup.find('.card_tie').text)
-    # TODO: Fix utf8 problem.
+    soup = BeautifulSoup(web.content, 'html.parser')
+    print(soup.find_all('div', class_='card_tie')
+          [0].text)
 
 if __name__ == '__main__':
     Calculate()
