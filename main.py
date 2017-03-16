a314c7875cb1e426a720baab3e2ac8617456a646
BalaWatermelon <jackson1994y+git@gmail.com>
2017-03-17 00:54:44 +0800

    add comment

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class Calculate():
     web = requests.get('http://bd.youxidudu.com/npc/20170102/4266.html')
     soup = BeautifulSoup(web.text.encode('utf8'), 'html.parser')
     print(soup.find('.card_tie').text)
+    # TODO: Fix utf8 problem.
 
 if __name__ == '__main__':
     Calculate()
